c0a703083d6b1e2789b5f6be1b9e3bb455b99fd9
Alexander Nikolaev <quattro.ex@gmail.com>
2021-07-21 09:13:46 +0300

    refactoring get data array favorites vacancies

--- src/containers/FavoritePage/FavoritePage.jsx
+++ src/containers/FavoritePage/FavoritePage.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import VacanciesList from '@components/VacanciesList'
 import VacancyDescription from '@components/VacancyDescription'
@@ -8,31 +9,22 @@ import s from './FavoritePage.module.sass';
 
 const FavoritePage = () => {
   const dataVacancy = useSelector(store => store.favoriteReducer)
+  const favoritesVacancies = useMemo(() => Object.values(dataVacancy), [dataVacancy])
   const vacancyId = useSelector(state => state.activeVacancyReducer)
-  const descriptionVacancy = dataVacancy[vacancyId.id]
   const descriptionIsOpen = useSelector(state => state.closeDescriptionReducer.isOpen)
+  const descriptionVacancy = dataVacancy[vacancyId.id]
   const windowWidth = useWindowDimensions().width
   const breakpoint = 1025
-
-  const data = []
-  const arrFromObj = () => {
-    for (let key in dataVacancy) {
-      data.push(dataVacancy[key])
-    }
-  }
-
-  arrFromObj()
 
   return (
     <section className={s.main}>
       {Object.keys(dataVacancy).length ? (
         <div>
-          <VacanciesList dataVacancies={data} />
-          {/* <VacancyDescription description={descriptionVacancy} /> */}
+          <VacanciesList dataVacancies={favoritesVacancies} />
           {windowWidth <= breakpoint ? descriptionIsOpen && <VacancyDescriptionModal description={descriptionVacancy} /> : <VacancyDescription description={descriptionVacancy} />}
         </div>
-      ) 
-      : 'Тут ничего нет'}
+      )
+        : 'Тут ничего нет'}
     </section>
   );
 }
